5a3b5bc1c5d1887e6a966b155f91d0fb30bd2926
정준영 <jyjeong620@naver.com>
2021-01-21 11:15:23 +0900

    feat: 자동차 대수와 시도횟수를 입력받는 메소드 생성

--- src/main/java/jyjeong/racingcar/InputView.java
+++ src/main/java/jyjeong/racingcar/InputView.java
@@ -1,5 +1,25 @@
 package jyjeong.racingcar;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    Scanner input = new Scanner(System.in);
+
+    /**
+     * 자동차 대수 입력받기
+     */
+    public int carCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return input.nextInt();
+    }
+
+    /**
+     * 시도 횟수 입력받기
+     */
+    public int tryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return input.nextInt();
+    }
 
 }
