4440fd5eb3bcfe33ca637600cebc2db86ae6231e
정준영 <jyjeong620@naver.com>
2021-01-21 12:53:37 +0900

    feat: 자동차 정보 저장하는 클래스

---
+++ src/main/java/jyjeong/racingcar/CarInfo.java
@@ -0,0 +1,16 @@
+package jyjeong.racingcar;
+
+public class CarInfo {
+
+    private final int CarMoveCount;
+    private int move;
+
+    public CarInfo(int CarMoveCount) {
+        this.CarMoveCount = CarMoveCount;
+    }
+
+
+    public int getCarMoveCount() {
+        return CarMoveCount;
+    }
+}
