25e9054bed73712e4ab99f107c0a01608c14c1c5
정준영 <jyjeong620@naver.com>
2021-01-21 15:27:03 +0900

    feat: 메소드 추가
    랜덤숫자생성 메소드
    랜덤값에 따른 이동여부 확인 매소드
    자동자 생성 메소드
    자동자 주행 메소드

--- src/main/java/jyjeong/racingcar/RacingRunner.java
+++ src/main/java/jyjeong/racingcar/RacingRunner.java
@@ -1,23 +1,62 @@
 package jyjeong.racingcar;
 
-public class RacingRunner {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-    private int carCount;
-    private int tryCount;
+public class RacingRunner {
+    private final static int MOVABLE_VALUE = 3;
+    private final static int RANDOM_RANGE = 10;
+    List<CarInfo> carInfos = new ArrayList<>();
     InputView inputView = new InputView();
     ResultView resultView = new ResultView();
 
     public void run() {
-        carCount = inputView.carCount();
-        tryCount = inputView.tryCount();
+        int carCount = this.inputView.carCount();
+        int tryCount = this.inputView.tryCount();
+        setCar(carCount);
+        for(int i = 0 ; i < tryCount ; i++){
+            racingCar();
+            System.out.println();
+        }
 
     }
 
-    //TODO 0~9랜덤한 숫자 생성하는 메소드
+    /**
+     * 0~9랜덤한 숫자 생성하는 메소드
+     * @return 랜덤 숫자
+     */
+    public int createRandom(){
+        Random random = new Random();
+        return random.nextInt(RANDOM_RANGE);
+    }
 
-    //TODO 숫자가 0~3 이면 멈추고 4~9면 움직이게하는 메소드 isMovable
+    /**
+     * 숫자가 0~3 이면 멈추고 4~9면 움직이게하는 메소드 isMovable
+     */
+    public void isMovable(CarInfo carInfo, int count){
+        if(count > MOVABLE_VALUE) {
+            carInfo.moveCar();
+        }
+    }
 
-    //TODO 입력받은 CarCount 만큼 CarInfo 생성
+    /**
+     * 입력받은 CarCount 만큼 CarInfo 생성
+     */
+    private void setCar(int carCount) {
+        for(int i = 0 ; i < carCount ; i++) {
+            CarInfo carInfo = new CarInfo();
+            this.carInfos.add(carInfo);
+        }
+    }
 
-    //TODO 입력받은 MoveCount 만큼 CarMove 후 출력
+    /**
+     * 입력받은 MoveCount 만큼 CarMove 후 출력
+     */
+    private void racingCar(){
+        for(CarInfo carInfo : this.carInfos){
+            isMovable(carInfo, createRandom());
+        }
+        this.resultView.printCar(this.carInfos);
+    }
 }
