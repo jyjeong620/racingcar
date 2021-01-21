5063f438af90b8ef312f180371a9b23f62a1f592
정준영 <jyjeong620@naver.com>
2021-01-21 15:27:35 +0900

    feat: 출력 클래스

--- src/main/java/jyjeong/racingcar/ResultView.java
+++ src/main/java/jyjeong/racingcar/ResultView.java
@@ -1,4 +1,25 @@
 package jyjeong.racingcar;
 
+import java.util.List;
+
 public class ResultView {
+
+    /**
+     * 전체 차량 이동 횟수 출력
+     */
+    public void printCar(List<CarInfo> carInfos) {
+        for(CarInfo carInfo : carInfos){
+            printMove(carInfo);
+            System.out.println();
+        }
+    }
+
+    /**
+     * 지정된 차의 이동횟수 출력
+     */
+    private void printMove(CarInfo carInfo) {
+        for(int i = 0 ; i < carInfo.getCarMoveCount() ; i++) {
+            System.out.print("-");
+        }
+    }
 }
