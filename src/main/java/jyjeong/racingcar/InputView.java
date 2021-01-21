ba9fd7c8cf562e6a901288344ba458ff9d07879f
정준영 <jyjeong620@naver.com>
2021-01-21 15:25:28 +0900

    refactor: 지역변수 this 추가

--- src/main/java/jyjeong/racingcar/InputView.java
+++ src/main/java/jyjeong/racingcar/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
      */
     public int carCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        return input.nextInt();
+        return this.input.nextInt();
     }
 
     /**
@@ -19,7 +19,7 @@ public class InputView {
      */
     public int tryCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        return input.nextInt();
+        return this.input.nextInt();
     }
 
 }
